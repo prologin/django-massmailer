f5754efe92f421ef5d18310402ae9b1b4b9c381d
Alexandre Macabies <web+git@zopieux.com>
2017-05-30 22:36:50 +0200

    mailing: tests: test batch send with celery mock

--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
 from django.urls import reverse
+from unittest import mock
 
 from prologin import tests
-from mailing import models
+from mailing import models, forms
 
 
-class ReportingTestCase(tests.WithStaffUserMixin, tests.WithSuperUserMixin, tests.WithContestantMixin, tests.ProloginTestCase):
+class TemplateTestCase(tests.WithContestantMixin, tests.ProloginTestCase):
     def setUp(self):
         super().setUp()
         self.plain_tpl = models.Template(
@@ -43,7 +44,7 @@ Hey, you are {% if user.email.startswith('test') %}<em>fake</em>{% else %}<stron
     def test_full_preview(self):
         context = {'user': self.contestant}
         self.assertDictEqual(self.plain_tpl.full_preview(context), {
-            'subject': {'error':  {'msg': "'edition' is undefined", 'type': 'undefined'}},
+            'subject': {'error': {'msg': "'edition' is undefined", 'type': 'undefined'}},
             'plain': {'content': "Hey, you are real.", 'context': {'declared': [], 'missing': []}},
             'html': {'content': '', 'context': {'declared': ['user'], 'missing': []}},
         })
@@ -58,6 +59,9 @@ Hey, you are {% if user.email.startswith('test') %}<em>fake</em>{% else %}<stron
             self.html_tpl.render(models.TemplateItem.html, context),
             "Hey, you are <strong>real</strong>.")
 
+
+class PermissionTestCase(tests.WithStaffUserMixin, tests.WithSuperUserMixin, tests.WithContestantMixin,
+                         tests.ProloginTestCase):
     def test_permissions(self):
         with self.user_login(self.contestant):
             response = self.client.get(reverse('mailing:dashboard'))
@@ -76,10 +80,56 @@ Hey, you are {% if user.email.startswith('test') %}<em>fake</em>{% else %}<stron
             self.assertValidResponse(response)
 
     def test_404(self):
-        self.plain_tpl.save()
-        self.plain_tpl.refresh_from_db()
-        tpl_pk = self.plain_tpl.pk
+        tpl = models.Template(name="foo")
+        tpl.save()
 
         with self.user_login(self.super_user):
-            self.assertInvalidResponse(self.client.get(reverse('mailing:template:update', args=[tpl_pk + 1])))
-            self.assertValidResponse(self.client.get(reverse('mailing:template:update', args=[tpl_pk])))
+            self.assertInvalidResponse(self.client.get(reverse('mailing:template:update', args=[tpl.pk + 1])))
+            self.assertValidResponse(self.client.get(reverse('mailing:template:update', args=[tpl.pk])))
+
+
+class BatchSendTestCase(tests.WithSuperUserMixin, tests.ProloginTestCase):
+    @mock.patch('mailing.tasks.send_email')
+    def test_send(self, send_email):
+        def mark_mail_as_sent(args, **kwargs):
+            # simulate celery sending the mail
+            pk, = args
+            email = models.BatchEmail.objects.get(pk=pk)
+            email.state = models.MailState.sent.value
+            email.save()
+
+        send_email.apply_async = mock.MagicMock()
+        send_email.apply_async.side_effect = mark_mail_as_sent
+
+        template = models.Template(
+            name="such template",
+            subject="salut {{ user }}",
+            plain_body="salut toi",
+        )
+        template.save()
+        query = models.Query(
+            name="much query",
+            query="ProloginUser as user",
+        )
+        query.save()
+
+        with self.user_login(self.super_user):
+            data = {'name': "this is my test batch", 'template': template.pk, 'query': query.pk}
+            # first send: should be greeted with "foolproof is required"
+            response = self.client.post(reverse('mailing:batch:new'), data)
+            self.assertFormError(response, 'form', 'foolproof', "This field is required.")
+            # next send: should be ok
+            data['foolproof'] = forms.CreateBatchForm.FOOLPROOF_PHRASE % {'n': 1}
+            response = self.client.post(reverse('mailing:batch:new'), data, follow=True)
+
+        self.assertValidResponse(response)
+        content = response.content.decode()
+        subject = template.render(models.TemplateItem.subject, {'user': self.super_user.username})
+        self.assertIn(data['name'], content)
+        self.assertIn(self.super_user.username, content)
+        self.assertIn(template.name, content)
+        self.assertIn(query.name, content)
+        self.assertIn(subject, content)
+
+        # are all mail sent?
+        self.assertTrue(models.Batch.objects.get().completed)
