651c9a6b1d2884292d5fb180091513f2361bc4f5
Antoine Pietri <antoine.pietri1@gmail.com>
2019-07-20 16:06:24 +0200

    setup.py: add classifiers

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     packages=["massmailer"],
     install_requires=[
         "bleach",  # HTML sanitizer
-        "celery<5",  # task queue
-        "django<3",
+        "celery>4",  # task queue
+        "django>=2.1",
         "django-bootstrap-breadcrumbs",  # breadcrumbs in default templates
         "django-reversion",  # model revision/history
         "django-crispy-forms",  # form builder compatible with Bootstrap
@@ -17,5 +17,16 @@ setup(
         "markdown",
         "pyparsing",  # query language parser
         "rules",  # fine-grained permissions
+    ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 4 - Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Email',
     ],
 )
